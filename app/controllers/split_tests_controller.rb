8a85488faf0381c13c2772e5ab544a7f9eec4e29
Daniel J. Pritchett <dpritchett@gmail.com>
2013-11-02 13:01:52 -0500

    UI tweaks

--- app/controllers/split_tests_controller.rb
+++ app/controllers/split_tests_controller.rb
@@ -6,8 +6,9 @@ class SplitTestsController < ApplicationController
 
   def tests
     {
-      "i_like"        => %w[apples bananas],
-      "i_do_not_like" => %w[apples bananas],
+      "i_like"          => %w[apples bananas],
+      "i_do_not_like"   => %w[apples bananas],
+      "i_prefer_to_pay" => ["$10 per month", "$100 per year"],
     }
   end
 end
