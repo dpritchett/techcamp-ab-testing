89fdd4590be89f1e89de75077bffd5be9aec3ad5
Daniel J. Pritchett <dpritchett@gmail.com>
2013-11-02 12:17:07 -0500

    Bugfix to previous

--- app/controllers/split_tests_controller.rb
+++ app/controllers/split_tests_controller.rb
@@ -1,12 +1,12 @@
 class SplitTestsController < ApplicationController
   def index
-    @test_name   = params[:test_name]
-    @test_values = self.class.tests[@test_name]
+    @test_name   = params[:test_name] || tests.keys.sample
+    @test_values = tests[@test_name]
   end
 
-  def self.tests
+  def tests
     {
-      "i_like"      => %w[apples bananas],
+      "i_like"        => %w[apples bananas],
       "i_do_not_like" => %w[apples bananas],
     }
   end
