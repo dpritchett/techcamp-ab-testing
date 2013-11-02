85b88d9b87a71ac4bd2719ccddafaa720b780d7b
Daniel J. Pritchett <dpritchett@gmail.com>
2013-11-02 12:15:31 -0500

    Broke up the tester to provide options

--- app/controllers/split_tests_controller.rb
+++ app/controllers/split_tests_controller.rb
@@ -1,4 +1,13 @@
 class SplitTestsController < ApplicationController
   def index
+    @test_name   = params[:test_name]
+    @test_values = self.class.tests[@test_name]
+  end
+
+  def self.tests
+    {
+      "i_like"      => %w[apples bananas],
+      "i_do_not_like" => %w[apples bananas],
+    }
   end
 end
